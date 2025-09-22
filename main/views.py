a87e5968f38f3b8469db92eeeed95c84ba164da1
le1zyyx <le1zyyx@gmail.com>
2025-09-22 21:54:37 +0300

    The website is in its final stages. Second attempt.

--- main/views.py
+++ main/views.py
@@ -10,25 +10,17 @@ def index(request):
     }
     return render(request, 'main/index.html', {'homepage': homepage, 'contacts': contacts})
 
-<<<<<<< HEAD
 def departments_list(request):
     departments = Department.objects.all()
-=======
-def departments(request):
-    departments = Department.objects.all() 
->>>>>>> 2674e7e98ce516c8dfc083ce720cbad698f76b82
     return render(request, 'main/departments.html', {'departments': departments})
 
 def specialties_list(request):
     programs = Program.objects.all()
     return render(request, 'main/specialties.html', {'programs': programs})
 
-<<<<<<< HEAD
 def lecturers_list(request):
     lecturers = Lecturer.objects.all()
     return render(request, 'main/lecturers.html', {'lecturers': lecturers})
-=======
->>>>>>> 2674e7e98ce516c8dfc083ce720cbad698f76b82
 
 def department_detail(request, department_id):
     department = get_object_or_404(Department, id=department_id)
